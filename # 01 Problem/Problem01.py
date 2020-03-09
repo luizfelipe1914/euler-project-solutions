f84d8872dfeee31f7f86431f67817267bef189f8
luizfelipe1914 <luizfelipe1914@gmail>
2020-03-09 13:42:53 -0300

    Solving of problem # 21.

---
+++ # 01 Problem/Problem01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#-*- encoding:UTF-8 -*-
+
+number = 1
+soma = 0
+
+while(number < 1000):
+    if(number % 3 == 0 or number % 5 == 0):
+        soma = soma + number
+    number = number + 1
+
+print("A soma dos múltiplos de 3 ou 5 no intervalo [1, 1000[ é:", soma)
